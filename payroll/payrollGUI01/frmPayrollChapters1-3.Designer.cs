eedf7ca959cf5842780ea5ccc474d88176a6e624
Jeff Scott <jpscott@ranken.edu>
2022-08-31 10:53:58 -0500

    Chapters 4 - 5 changes added

--- payroll/payrollGUI01/frmPayrollChapters1-3.Designer.cs
+++ payroll/payrollGUI01/frmPayrollChapters1-3.Designer.cs
@@ -41,12 +41,22 @@
             this.lblRate = new System.Windows.Forms.Label();
             this.txtGross = new System.Windows.Forms.TextBox();
             this.lblGross = new System.Windows.Forms.Label();
+            this.txtTotalNumberGrossPays = new System.Windows.Forms.TextBox();
+            this.lblTotalNumberGrossPays = new System.Windows.Forms.Label();
+            this.txtTotalGrossPay = new System.Windows.Forms.TextBox();
+            this.lblTotalGrossPay = new System.Windows.Forms.Label();
+            this.txtAverageGrossPay = new System.Windows.Forms.TextBox();
+            this.lblAverageGrossPay = new System.Windows.Forms.Label();
+            this.txtHighestGrossPay = new System.Windows.Forms.TextBox();
+            this.lblHighestGrossPay = new System.Windows.Forms.Label();
+            this.txtLowestGrossPay = new System.Windows.Forms.TextBox();
+            this.lblLowestGrossPay = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCalculate
             // 
             this.btnCalculate.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalculate.Location = new System.Drawing.Point(53, 335);
+            this.btnCalculate.Location = new System.Drawing.Point(247, 378);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(160, 54);
             this.btnCalculate.TabIndex = 4;
@@ -58,7 +68,7 @@
             // 
             this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClear.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(247, 335);
+            this.btnClear.Location = new System.Drawing.Point(441, 378);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(160, 54);
             this.btnClear.TabIndex = 5;
@@ -69,7 +79,7 @@
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(443, 335);
+            this.btnExit.Location = new System.Drawing.Point(637, 378);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(160, 54);
             this.btnExit.TabIndex = 6;
@@ -80,17 +90,17 @@
             // lblFirstName
             // 
             this.lblFirstName.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFirstName.Location = new System.Drawing.Point(50, 9);
+            this.lblFirstName.Location = new System.Drawing.Point(50, 35);
             this.lblFirstName.Name = "lblFirstName";
             this.lblFirstName.Size = new System.Drawing.Size(163, 36);
-            this.lblFirstName.TabIndex = 8;
+            this.lblFirstName.TabIndex = 13;
             this.lblFirstName.Text = "First Name:";
             this.lblFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtFirstName
             // 
             this.txtFirstName.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFirstName.Location = new System.Drawing.Point(247, 12);
+            this.txtFirstName.Location = new System.Drawing.Point(247, 38);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(160, 38);
             this.txtFirstName.TabIndex = 0;
@@ -98,7 +108,7 @@
             // txtLastName
             // 
             this.txtLastName.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLastName.Location = new System.Drawing.Point(247, 75);
+            this.txtLastName.Location = new System.Drawing.Point(247, 101);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(160, 38);
             this.txtLastName.TabIndex = 1;
@@ -106,17 +116,17 @@
             // lblLastName
             // 
             this.lblLastName.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLastName.Location = new System.Drawing.Point(50, 72);
+            this.lblLastName.Location = new System.Drawing.Point(50, 98);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.Size = new System.Drawing.Size(163, 36);
-            this.lblLastName.TabIndex = 9;
+            this.lblLastName.TabIndex = 14;
             this.lblLastName.Text = "Last Name:";
             this.lblLastName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtHours
             // 
             this.txtHours.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtHours.Location = new System.Drawing.Point(247, 144);
+            this.txtHours.Location = new System.Drawing.Point(247, 170);
             this.txtHours.Name = "txtHours";
             this.txtHours.Size = new System.Drawing.Size(160, 38);
             this.txtHours.TabIndex = 2;
@@ -124,17 +134,17 @@
             // lblHours
             // 
             this.lblHours.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHours.Location = new System.Drawing.Point(50, 141);
+            this.lblHours.Location = new System.Drawing.Point(50, 167);
             this.lblHours.Name = "lblHours";
             this.lblHours.Size = new System.Drawing.Size(163, 36);
-            this.lblHours.TabIndex = 10;
+            this.lblHours.TabIndex = 15;
             this.lblHours.Text = "Hours:";
             this.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtRate
             // 
             this.txtRate.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRate.Location = new System.Drawing.Point(247, 207);
+            this.txtRate.Location = new System.Drawing.Point(247, 233);
             this.txtRate.Name = "txtRate";
             this.txtRate.Size = new System.Drawing.Size(160, 38);
             this.txtRate.TabIndex = 3;
@@ -142,17 +152,17 @@
             // lblRate
             // 
             this.lblRate.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRate.Location = new System.Drawing.Point(50, 204);
+            this.lblRate.Location = new System.Drawing.Point(50, 230);
             this.lblRate.Name = "lblRate";
             this.lblRate.Size = new System.Drawing.Size(163, 36);
-            this.lblRate.TabIndex = 11;
+            this.lblRate.TabIndex = 16;
             this.lblRate.Text = "Rate:";
             this.lblRate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtGross
             // 
             this.txtGross.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGross.Location = new System.Drawing.Point(247, 267);
+            this.txtGross.Location = new System.Drawing.Point(247, 293);
             this.txtGross.Name = "txtGross";
             this.txtGross.ReadOnly = true;
             this.txtGross.Size = new System.Drawing.Size(160, 38);
@@ -161,12 +171,107 @@
             // lblGross
             // 
             this.lblGross.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGross.Location = new System.Drawing.Point(50, 264);
+            this.lblGross.Location = new System.Drawing.Point(50, 290);
             this.lblGross.Name = "lblGross";
             this.lblGross.Size = new System.Drawing.Size(163, 36);
-            this.lblGross.TabIndex = 12;
+            this.lblGross.TabIndex = 17;
             this.lblGross.Text = "Gross:";
             this.lblGross.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtTotalNumberGrossPays
+            // 
+            this.txtTotalNumberGrossPays.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalNumberGrossPays.Location = new System.Drawing.Point(725, 35);
+            this.txtTotalNumberGrossPays.Name = "txtTotalNumberGrossPays";
+            this.txtTotalNumberGrossPays.ReadOnly = true;
+            this.txtTotalNumberGrossPays.Size = new System.Drawing.Size(160, 38);
+            this.txtTotalNumberGrossPays.TabIndex = 8;
+            // 
+            // lblTotalNumberGrossPays
+            // 
+            this.lblTotalNumberGrossPays.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalNumberGrossPays.Location = new System.Drawing.Point(454, 35);
+            this.lblTotalNumberGrossPays.Name = "lblTotalNumberGrossPays";
+            this.lblTotalNumberGrossPays.Size = new System.Drawing.Size(253, 36);
+            this.lblTotalNumberGrossPays.TabIndex = 18;
+            this.lblTotalNumberGrossPays.Text = "Total # Gross Pays:";
+            this.lblTotalNumberGrossPays.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtTotalGrossPay
+            // 
+            this.txtTotalGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalGrossPay.Location = new System.Drawing.Point(725, 98);
+            this.txtTotalGrossPay.Name = "txtTotalGrossPay";
+            this.txtTotalGrossPay.ReadOnly = true;
+            this.txtTotalGrossPay.Size = new System.Drawing.Size(160, 38);
+            this.txtTotalGrossPay.TabIndex = 9;
+            // 
+            // lblTotalGrossPay
+            // 
+            this.lblTotalGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalGrossPay.Location = new System.Drawing.Point(454, 98);
+            this.lblTotalGrossPay.Name = "lblTotalGrossPay";
+            this.lblTotalGrossPay.Size = new System.Drawing.Size(253, 36);
+            this.lblTotalGrossPay.TabIndex = 19;
+            this.lblTotalGrossPay.Text = "Total Gross Pay:";
+            this.lblTotalGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtAverageGrossPay
+            // 
+            this.txtAverageGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAverageGrossPay.Location = new System.Drawing.Point(725, 167);
+            this.txtAverageGrossPay.Name = "txtAverageGrossPay";
+            this.txtAverageGrossPay.ReadOnly = true;
+            this.txtAverageGrossPay.Size = new System.Drawing.Size(160, 38);
+            this.txtAverageGrossPay.TabIndex = 10;
+            // 
+            // lblAverageGrossPay
+            // 
+            this.lblAverageGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverageGrossPay.Location = new System.Drawing.Point(454, 167);
+            this.lblAverageGrossPay.Name = "lblAverageGrossPay";
+            this.lblAverageGrossPay.Size = new System.Drawing.Size(253, 36);
+            this.lblAverageGrossPay.TabIndex = 20;
+            this.lblAverageGrossPay.Text = "Average Gross Pay:";
+            this.lblAverageGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtHighestGrossPay
+            // 
+            this.txtHighestGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHighestGrossPay.Location = new System.Drawing.Point(725, 239);
+            this.txtHighestGrossPay.Name = "txtHighestGrossPay";
+            this.txtHighestGrossPay.ReadOnly = true;
+            this.txtHighestGrossPay.Size = new System.Drawing.Size(160, 38);
+            this.txtHighestGrossPay.TabIndex = 11;
+            // 
+            // lblHighestGrossPay
+            // 
+            this.lblHighestGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighestGrossPay.Location = new System.Drawing.Point(454, 239);
+            this.lblHighestGrossPay.Name = "lblHighestGrossPay";
+            this.lblHighestGrossPay.Size = new System.Drawing.Size(253, 36);
+            this.lblHighestGrossPay.TabIndex = 21;
+            this.lblHighestGrossPay.Text = "Highest Gross Pay:";
+            this.lblHighestGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtLowestGrossPay
+            // 
+            this.txtLowestGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLowestGrossPay.Location = new System.Drawing.Point(725, 300);
+            this.txtLowestGrossPay.Name = "txtLowestGrossPay";
+            this.txtLowestGrossPay.ReadOnly = true;
+            this.txtLowestGrossPay.Size = new System.Drawing.Size(160, 38);
+            this.txtLowestGrossPay.TabIndex = 12;
+            // 
+            // lblLowestGrossPay
+            // 
+            this.lblLowestGrossPay.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowestGrossPay.Location = new System.Drawing.Point(454, 300);
+            this.lblLowestGrossPay.Name = "lblLowestGrossPay";
+            this.lblLowestGrossPay.Size = new System.Drawing.Size(253, 36);
+            this.lblLowestGrossPay.TabIndex = 22;
+            this.lblLowestGrossPay.Text = "Lowest Gross Pay:";
+            this.lblLowestGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Form1
             // 
@@ -175,7 +280,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.CancelButton = this.btnClear;
-            this.ClientSize = new System.Drawing.Size(684, 450);
+            this.ClientSize = new System.Drawing.Size(1019, 481);
+            this.Controls.Add(this.txtLowestGrossPay);
+            this.Controls.Add(this.lblLowestGrossPay);
+            this.Controls.Add(this.txtHighestGrossPay);
+            this.Controls.Add(this.lblHighestGrossPay);
+            this.Controls.Add(this.txtAverageGrossPay);
+            this.Controls.Add(this.lblAverageGrossPay);
+            this.Controls.Add(this.txtTotalGrossPay);
+            this.Controls.Add(this.lblTotalGrossPay);
+            this.Controls.Add(this.txtTotalNumberGrossPays);
+            this.Controls.Add(this.lblTotalNumberGrossPays);
             this.Controls.Add(this.txtGross);
             this.Controls.Add(this.lblGross);
             this.Controls.Add(this.txtRate);
@@ -212,6 +327,16 @@
         private System.Windows.Forms.Label lblRate;
         private System.Windows.Forms.TextBox txtGross;
         private System.Windows.Forms.Label lblGross;
+        private System.Windows.Forms.TextBox txtTotalNumberGrossPays;
+        private System.Windows.Forms.Label lblTotalNumberGrossPays;
+        private System.Windows.Forms.TextBox txtTotalGrossPay;
+        private System.Windows.Forms.Label lblTotalGrossPay;
+        private System.Windows.Forms.TextBox txtAverageGrossPay;
+        private System.Windows.Forms.Label lblAverageGrossPay;
+        private System.Windows.Forms.TextBox txtHighestGrossPay;
+        private System.Windows.Forms.Label lblHighestGrossPay;
+        private System.Windows.Forms.TextBox txtLowestGrossPay;
+        private System.Windows.Forms.Label lblLowestGrossPay;
     }
 }
 
--- payroll/payrollGUI01/frmPayrollChapters1-3.cs
+++ payroll/payrollGUI01/frmPayrollChapters1-3.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/*
+ * 
+ *      TEST DATA:
+ *      =========
+ *      Person 1:   hours 20    rate 20     gross  400
+ *      Person 2:   hours 30    rate 20     gross  600
+ *      Person 3:   hours 10    rate 20     gross  200
+ *      Person 4:   hours 50    rate 10     gross  550
+ *      Person 5:   hours 60    rate 30     gross 2100
+ *      
+ *      EXPECTED RESULTS:
+ *      =================
+ *      Total # Gross Pays: 5
+ *      Total gross pay:    3850
+ *      Average gross pay:   770
+ *      Highest gross pay:  2100
+ *      Lowest  gross pay:   200
+ */
 
 namespace payrollGUI01
 {
@@ -17,12 +29,69 @@ namespace payrollGUI01
             InitializeComponent();
         }
 
+        //  Program constants
+        const decimal MAXNONOT = 40.0m;         //  Max hours worked with no OT
+        const decimal OTRATE   =  1.5m;         //  Overtime rate
+
+        //  Class-Level variables
+        int totalNumberGrossPays = 0;           //  Total number of gross pays calculated
+        decimal totalGrossPay    = 0.0m;        //  Total of all gross pays
+        decimal averageGrossPay  = 0.0m;        //  Average gross pay
+        decimal highestGrossPay  = -1000m;      //  Highest gross pay
+        decimal lowestGrossPay   = 11111111m;   //  Lowest  gross pay
+
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             decimal hours = Convert.ToDecimal(txtHours.Text);
             decimal rate  = Convert.ToDecimal(txtRate.Text);
-            decimal gross = hours * rate;
+            decimal gross = 0m;
+
+            //  Calculate gross pay, first check for no OT worked
+            if (hours <= MAXNONOT)  // Worked 0 - 40 hours NO Overtime
+            {
+                gross = hours * rate;
+            }
+            else
+            {                       // Worked > 40 hour have Overtime
+                gross = ((MAXNONOT * rate)   +                  //  straight time (40 hours)
+                         ((hours - MAXNONOT) * rate * OTRATE)); //  overtime (> 40 hours)
+            }
+
+            //  Set associated textbox
             txtGross.Text = gross.ToString("c");
+
+            //  Increment totalNumberGrossPays
+            //  Set associated textbox
+            ++totalNumberGrossPays;
+            txtTotalNumberGrossPays.Text = totalNumberGrossPays.ToString();
+
+            //  Add current gross pay to totalGrossPay
+            //  Set associated textbox
+            totalGrossPay += gross;
+            txtTotalGrossPay.Text = totalGrossPay.ToString("c");
+
+            //  Calculate average gross pay
+            //  Set associated textbox
+            averageGrossPay = totalGrossPay / totalNumberGrossPays;
+            txtAverageGrossPay.Text = averageGrossPay.ToString("c");
+
+            //  Calculate highest gross Pay
+            if (gross > highestGrossPay)
+            {
+                highestGrossPay = gross;
+            }
+
+            //  Set associated textbox
+            txtHighestGrossPay.Text = highestGrossPay.ToString("c");
+
+            //  Calculate lowest gross Pay
+            if (gross < lowestGrossPay)
+            {
+                lowestGrossPay = gross;
+            }
+
+            //  Set associated textbox
+            txtLowestGrossPay.Text = lowestGrossPay.ToString("c");
         }
 
         private void btnClear_Click(object sender, EventArgs e)
